4cfc43a52f346ce69826fe81a01d5ff642479959
Sofia Murillo <sofia.mur@lvjusd.k12.ca.us>
2020-01-23 16:29:57 -0800

    swaer

--- min_bot.py
+++ min_bot.py
@@ -25,5 +25,46 @@ async def roll(ctx, number=20):
     await ctx.send("{res}\n(Rolled {num}, got {res})".format(num=number,
                                                              res=random.randint(1, number)))
 
+@bot.command(name="swear")
+async def swear (ctx, prompt):
+	sCount = 0
+	aCount = 0
+
+	if "s" in prompt:
+	  if "h" in prompt:
+		if "i" in prompt:
+		  if "t" in prompt:
+			await ctx.send("shit")
+	if "a" in prompt:
+	  if "s" in prompt:
+		for letter in prompt:
+		  if letter is 's':
+			sCount +=1
+		if sCount >= 2:
+		  await ctx.send("ass")
+	if "b" in prompt:
+	  if "i" in prompt:
+		if "t" in prompt:
+		  if "c" in prompt:
+			if "h" in prompt:
+			  await ctx.send("bitch")
+	  if "a" in prompt:
+		for letter in prompt:
+		  if letter is 'a':
+			aCount +=1
+		if "s" in prompt:
+		  if "t" in prompt:
+			if aCount >= 2:
+			  if "r" in prompt:
+				if "d" in prompt:
+				  await ctx.send("bastard")
+	if "f" in prompt:
+	  if "u" in prompt:
+		if "c" in prompt:
+		  if "k" in prompt:
+			await ctx.send("fuck")
+	else:
+	  await ctx.send("finished")
+
 with open("secret.txt","r") as secret_file:
     bot.run(secret_file.readline()[:-1])
